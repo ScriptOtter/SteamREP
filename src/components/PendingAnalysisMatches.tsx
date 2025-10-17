4e42c7e1f80e701e2604626f629a86bda4f3dbd0
Владлен Фролов <mainfrolov@gmail.com>
2025-10-17 20:25:37 +0300

    .

--- src/components/PendingAnalysisMatches.tsx
+++ src/components/PendingAnalysisMatches.tsx
@@ -32,7 +32,7 @@ export const PendingAnalysisMatches = () => {
         }));
       }
     };
-    const socket = io(process.env.SOCKET_PORT);
+    const socket = io(process.env.VITE_SOCKET_PORT);
     socket.on("trackingStats", handleTrackingStats);
     socket.emit("trackingStats");
     return () => {
